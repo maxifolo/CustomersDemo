11856c6c00a8ce3836b37843e0ce543b7a95bd1b
Maxi Follonier <maxifollonier@gmail.com>
2021-07-17 14:47:17 -0300

    Creacion DbContext terminada

---
+++ CustomersDemo.Data/CustomersDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using CustomersDemo.Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomersDemo.Data
+{
+    public class CustomersDemoContext : DbContext
+    {
+        public CustomersDemoContext(DbContextOptions dbContextOptions) : base(dbContextOptions) {}
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
+
